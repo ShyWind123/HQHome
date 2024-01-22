d1a53b7e96ce007523fe07bdafbed78585b01f60
ShyWind <2513561399@qq.com>
2024-01-22 19:24:35 +0800

    v0.3.0

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,12 +1,12 @@
 <template>
-  <img :src="bgPath" id="background-img">
+  <img :src="bgURL" id="background-img">
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive, onMounted } from 'vue'
 
 const bgNum = 6
-const bgPath = '/public/backgrounds/background' + Math.floor(Math.random() * bgNum + 1) + '.jpg'
+const bgURL = `/backgrounds/background${Math.floor(Math.random() * bgNum + 1)}.jpg`
 
 </script>
 
