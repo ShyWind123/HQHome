f4d3e1055478cbd929b68deb8fb553735631a675
ShyWind <2513561399@qq.com>
2024-04-28 21:26:12 +0800

    1.1.0

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,5 @@
+import axios from "axios";  
+
 export const getHitokoto = async () => {
   const res = await fetch("https://v1.hitokoto.cn");
   return await res.json();
@@ -16,4 +18,22 @@ export const getWeather = async (key: string, adcode: string) => {
 export const getTodayHistory = async () => {
   const res = await fetch("https://api.leafone.cn/api/lishi?type=rand")
   return await res.json();
+}
+
+export const getBackground = async () => {
+  axios.request({
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN',
+    headers: { 
+      'Cookie': 'MUID=251DE2E9DA606F9E19BCF686DBB26E16; SRCHD=AF=NOFORM; SRCHHPGUSR=SRCHLANG=zh-Hans; SRCHUID=V=2&GUID=BFB0BFA52C104F7B8FACE815E836BFD5&dmnchg=1; SRCHUSR=DOB=20240428; _EDGE_S=F=1&SID=1E907BB86D876A5E30946FD76C556B78; _EDGE_V=1; _SS=SID=1E907BB86D876A5E30946FD76C556B78; MUIDB=251DE2E9DA606F9E19BCF686DBB26E16'
+    }
+  })
+  .then((response) => {
+    return response.data.images[0].url;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+  return null;
 }
--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,13 +1,20 @@
 <template>
-  <img :src="bgURL" id="background-img">
+  <img id="background-img">
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive, onMounted } from 'vue'
+// import { getBackground } from '../api/api'
 
-const bgNum = 6
-const bgURL = `/backgrounds/background${Math.floor(Math.random() * bgNum + 1)}.jpg`
+// let bgURL = `/backgrounds/default.jpg`
+// const base = 'https://cn.bing.com'
 
+// onMounted(async () => {
+//   const result = await getBackground();
+//   if (result !== null) {
+//     bgURL = base + result;
+//   }
+// })
 </script>
 
 <style scoped>
@@ -16,5 +23,7 @@ const bgURL = `/backgrounds/background${Math.floor(Math.random() * bgNum + 1)}.j
   left: 0;
   top: 0;
   z-index: -1;
+  background: url(https://api.oneneko.com/v1/bing_today) no-repeat center center;
+  background-blend-mode: multiply;
 }
 </style>
