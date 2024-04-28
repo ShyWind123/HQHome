1eeb93c0911efdaf14f7c192d7405a5c3392d2fc
ShyWind <2513561399@qq.com>
2024-04-28 23:59:34 +0800

    1.1.1

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,20 +1,10 @@
 <template>
-  <img id="background-img">
+  <img :src="url" id="background-img">
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive, onMounted } from 'vue'
-// import { getBackground } from '../api/api'
-
-// let bgURL = `/backgrounds/default.jpg`
-// const base = 'https://cn.bing.com'
-
-// onMounted(async () => {
-//   const result = await getBackground();
-//   if (result !== null) {
-//     bgURL = base + result;
-//   }
-// })
+const url = 'https://api.oneneko.com/v1/bing_today'
 </script>
 
 <style scoped>
@@ -23,7 +13,5 @@ import { ref, reactive, onMounted } from 'vue'
   left: 0;
   top: 0;
   z-index: -1;
-  background: url(https://api.oneneko.com/v1/bing_today) no-repeat center center;
-  background-blend-mode: multiply;
 }
 </style>
